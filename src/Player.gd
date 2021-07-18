ee3973969b87affc392f3b2db219cd505a2628a3
Nacho Garcia Segovia <nacho.gs@zaleos.net>
2021-07-18 16:58:54 +0200

    added Mob scene

--- src/Player.gd
+++ src/Player.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
+signal hit
+
 
 # Declare member variables here. Examples:
 export var speed = 400  # How fast the player will move (pixels/sec).
@@ -9,6 +11,7 @@ var screen_size  # Size of the game window.
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_viewport_rect().size
+	hide()
 	
 func _process(delta):
 	var velocity = Vector2()  # The player's movement vector.
@@ -29,8 +32,29 @@ func _process(delta):
 	position += velocity * delta
 	position.x = clamp(position.x, 0, screen_size.x)
 	position.y = clamp(position.y, 0, screen_size.y)
+	
+	if velocity.x != 0:
+		$AnimatedSprite.animation = "walk"
+		$AnimatedSprite.flip_v = false
+		# See the note below about boolean assignment
+		$AnimatedSprite.flip_h = velocity.x < 0
+	elif velocity.y != 0:
+		$AnimatedSprite.animation = "up"
+		$AnimatedSprite.flip_v = velocity.y > 0
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+func _on_Player_body_entered(body):
+	hide()  # Player disappears after being hit.
+	emit_signal("hit")
+	$CollisionShape2D.set_deferred("disabled", true)
+	
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
+
